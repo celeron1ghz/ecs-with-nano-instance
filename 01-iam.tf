2823e4925d12e5d6b880749af88b59bb422d45b7
celeron1ghz <celeron1ghz@gmail.com>
2023-02-17 14:00:24 +0900

    attach maganed roles

--- 01-iam.tf
+++ 01-iam.tf
@@ -15,6 +15,16 @@ resource "aws_iam_role" "app" {
   assume_role_policy = data.aws_iam_policy_document.app.json
 }
 
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.app.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs" {
+  role       = aws_iam_role.app.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
 resource "aws_iam_instance_profile" "app" {
   name = "${local.appid}-app"
   role = aws_iam_role.app.name
