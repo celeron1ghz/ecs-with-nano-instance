52ca5278ab0e3d7d8d3ec1fc45f6eee9a1aebfd6
celeron1ghz <celeron1ghz@gmail.com>
2023-02-28 17:22:04 +0900

    add ecs task execution role

--- 01-iam.tf
+++ 01-iam.tf
@@ -1,3 +1,4 @@
+## instance role
 data "aws_iam_policy_document" "app" {
   statement {
     effect  = "Allow"
@@ -28,4 +29,41 @@ resource "aws_iam_role_policy_attachment" "ecs" {
 resource "aws_iam_instance_profile" "app" {
   name = "${local.appid}-app"
   role = aws_iam_role.app.name
+}
+
+## ecs task execution role
+data "aws_iam_policy_document" "ecs-task" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "loggroup" {
+  statement {
+    effect    = "Allow"
+    actions   = ["logs:CreateLogGroup"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "ecs-task" {
+  name               = "${local.appid}-ecs-task"
+  description        = "${local.appid} ecs task execution role"
+  assume_role_policy = data.aws_iam_policy_document.ecs-task.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task" {
+  role       = aws_iam_role.ecs-task.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy" "loggroup" {
+  name   = "${local.appid}-ecs-task-loggroup"
+  role   = aws_iam_role.ecs-task.name
+  policy = data.aws_iam_policy_document.loggroup.json
 }
