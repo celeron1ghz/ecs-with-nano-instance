f231d4a6b940c6ae1bd9d5e4d8784ea3bb6a4871
celeron1ghz <celeron1ghz@gmail.com>
2023-03-15 10:46:00 +0900

    renamed

--- runner/01-build_container.sh
+++ runner/01-build_container.sh
@@ -1,3 +1,7 @@
 # docker run -it perl:5.36.0-slim-bullseye /bin/bash
 # docker run -p 5000:5000 -it test /bin/bash
+
+BASE="$(dirname $(realpath $0))"
+cd "$BASE/../docker"
+
 docker build . -t test:latest
