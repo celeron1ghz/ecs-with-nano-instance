63e108860afd73afb1560b61fd8cc5068861f100
celeron1ghz <celeron1ghz@gmail.com>
2023-03-16 14:56:18 +0900

    arch setting in env

--- tf/00-var.tf
+++ tf/00-var.tf
@@ -4,10 +4,17 @@ locals {
   // getting latest amazonlinux2 ami id
   // aws ssm get-parameters --names /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2 --query "Parameters[].[Value, LastModifiedDate, Name]" --output text
 
+  // getting x86 ecs optimized latest amazonlinux2 ami id
+  // aws ssm get-parameters --names /aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id --query "Parameters[].[Value, LastModifiedDate, Name]" --output text
+
   // getting arm64 ecs optimized latest amazonlinux2 ami id
   // aws ssm get-parameters --names /aws/service/ecs/optimized-ami/amazon-linux-2/arm64/recommended/image_id --query "Parameters[].[Value, LastModifiedDate, Name]" --output text
-  image_id      = "ami-000f45a90a4044e1f"
-  instance_type = "t4g.nano"
+  # x86
+  image_id      = "ami-01703abf1e5cede54"
+  instance_type = "t3.nano"
+  # arm64
+  # image_id      = "ami-000f45a90a4044e1f"
+  # instance_type = "t4g.nano"
 }
 
 # terraform {
