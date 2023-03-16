fca87ecf546edc4c44b8e45317c6d3c88eae6f76
celeron1ghz <celeron1ghz@gmail.com>
2023-03-16 16:01:47 +0900

    changed variable name

--- runner/03-deploy_to_ecs.sh
+++ runner/03-deploy_to_ecs.sh
@@ -7,7 +7,7 @@ GROUP_B="acceptessa2-app-b"
 GROUP_A_COUNT=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $GROUP_A --query "length(AutoScalingGroups[0].Instances)"`
 GROUP_B_COUNT=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $GROUP_B --query "length(AutoScalingGroups[0].Instances)"`
 
-LATEST_DOCKER_IMAGE=`aws ecr list-images --repository-name $CONTAINER_IMAGE_NAME --query "imageIds[0].imageTag" --output text`
+LATEST_DOCKER_IMAGE=`aws ecr list-images --repository-name $DOCKER_CONTAINER_IMAGE_NAME --query "imageIds[0].imageTag" --output text`
 
 if [ $GROUP_A_COUNT -ge 1 ] && [ $GROUP_B_COUNT -ge 1 ]; then
     echo "ERROR: both group instance are exist"
@@ -36,10 +36,10 @@ echo "green is $GREEN_GROUP, blue is $BLUE_GROUP"
 echo "BLUE instance 0 -> 1"
 aws autoscaling set-desired-capacity --auto-scaling-group-name $BLUE_GROUP --desired-capacity 1
 echo "BLUE service 0 -> 1"
-GROUP_NAME=$BLUE_SYM  DOCKER_IMAGE=$CONTAINER_IMAGE_NAME DOCKER_TAG=$LATEST_DOCKER_IMAGE ecspresso --config="$PWD/../ecspresso/ecspresso.yml" deploy --tasks=1
+ASG_GROUP_SYM=$BLUE_SYM  DOCKER_IMAGE_NAME=$DOCKER_CONTAINER_IMAGE_NAME DOCKER_IMAGE_TAG=$LATEST_DOCKER_IMAGE ecspresso --config="$PWD/../ecspresso/ecspresso.yml" deploy --tasks=1
 
 ## IN GREEN GROUP: stop ecs service and shutdown instance
 echo "GREEN service 1 -> 0"
-GROUP_NAME=$GREEN_SYM ecspresso --config="$PWD/../ecspresso/ecspresso.yml" scale --tasks=0
+ASG_GROUP_SYM=$GREEN_SYM ecspresso --config="$PWD/../ecspresso/ecspresso.yml" scale --tasks=0
 echo "GREEN instance 1 -> 0"
 aws autoscaling set-desired-capacity --auto-scaling-group-name $GREEN_GROUP --desired-capacity 0
